138dde7560a915f818923974db8f5491cc6fddfe
Bartosz Bańczerowski <bartoszbanczerowski@gmail.com>
2017-01-22 23:26:58 +0100

    Deleted settings from menu in MainActivity.

--- app/src/main/java/eu/mobilebear/imagegallery/MainActivity.java
+++ app/src/main/java/eu/mobilebear/imagegallery/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.FrameLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -56,22 +54,6 @@ public class MainActivity extends AppCompatActivity {
     fragmentManager.beginTransaction()
         .add(R.id.container, PhotosFragment.newInstance(), PHOTOS_FRAGMENT)
         .commit();
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-
-    int id = item.getItemId();
-    if (id == R.id.action_settings) {
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
   }
 
   private void initializePhotoComponent(Activity activity) {
